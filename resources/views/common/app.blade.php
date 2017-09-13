afa0842342afcc30c165c94e47b2777510623f90
longzongqin <longzongqin@foxmail.com>
2017-09-13 23:27:47 +0800

    百度推送

--- resources/views/common/app.blade.php
+++ resources/views/common/app.blade.php
@@ -207,6 +207,7 @@
         var bp = document.createElement('script');
         var curProtocol = window.location.protocol.split(':')[0];
         if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
         }
         else {
             bp.src = 'http://push.zhanzhang.baidu.com/push.js';
@@ -215,7 +216,6 @@
         s.parentNode.insertBefore(bp, s);
     })();
 </script>
-
 
 </body>
 </html>
